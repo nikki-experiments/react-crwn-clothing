f20c752e6182a24fd183e6f839526dd6e11c05b8
Nikki Purcell <nikki.purcell@chownow.com>
2023-08-07 19:49:46 -0700

    Add all firebase GET request for db data

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -13,7 +13,11 @@ import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs
 } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -42,6 +46,39 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 // Instantiate database from Firestore
 export const db = getFirestore();
+
+// Create a collection (used to add categories in Cloud Firestore - once only)
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  field
+  ) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  // for each object in array, create a new docRef where key is the title and value is the object.
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object[field].toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+}
 
 // Create method to create new user document in Firestore for each user who authenicates.
 export const createUserDocumentFromAuth = async (
